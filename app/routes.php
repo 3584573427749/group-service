8db5e9e251fa7ae667dd95e3f678891f6395b33d
Kjell <kjell@kejpa.com>
2026-07-19 21:18:25 +0300

    Uppdatera grouplevel

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Http\Actions\GroupLevel\CreateGroupLevelAction;
 use App\Http\Actions\GroupLevel\GetAllGroupLevelsAction;
 use App\Http\Actions\GroupLevel\GetGroupLevelAction;
+use App\Http\Actions\GroupLevel\UpdateGroupLevelAction;
 use Slim\App;
 
 return function (App $app) : void {
@@ -12,4 +13,5 @@ return function (App $app) : void {
     $app->post('/group-levels', CreateGroupLevelAction::class);
     $app->get('/group-levels', GetAllGroupLevelsAction::class);
     $app->get('/group-levels/{id}', GetGroupLevelAction::class);
+    $app->put('/group-levels/{id}', UpdateGroupLevelAction::class);
 };
---
+++ src/Http/Actions/GroupLevel/UpdateGroupLevelAction.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Actions\GroupLevel;
+
+use App\Application\Commands\UpdateGroupLevelCommand;
+use App\Application\Handlers\UpdateGroupLevelHandler;
+use App\Application\Validators\UpdateGroupLevelValidator;
+use App\Domain\Exception\ValidationException;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Log\LoggerInterface;
+
+class UpdateGroupLevelAction extends GroupLevelAction {
+    public function __construct(LoggerInterface $logger, private UpdateGroupLevelHandler $handler) {
+        parent::__construct($logger);
+    }
+
+    protected function action() : Response {
+        $id = $this->request->getAttribute('id');
+        $data = (array)$this->request->getParsedBody();
+        $data['groupLevelId'] = $id;
+
+        $errors = UpdateGroupLevelValidator::validate($data);
+        if (count($errors) > 0) {
+            throw new ValidationException('Felaktig indata', $errors);
+        }
+
+        $groupLevelCommand = UpdateGroupLevelCommand::fromRequest($data);
+        $dto = $this->handler->handle($groupLevelCommand);
+
+        return $this->respondWithData($dto);
+
+    }
+}
